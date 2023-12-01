8785adaf6a830ff90cb289e66f620115d807c212
Johnny Nguyen <thisisjohnny@gmail.com>
2023-12-01 14:54:34 -0600

    Solution to Day 1 - Part 2

--- calibration-puzzle/solve-puzzle.ts
+++ calibration-puzzle/solve-puzzle.ts
@@ -1,19 +1,69 @@
 import * as fs from 'fs';
 
+interface NumberWordMap {
+  [key: string]: number;
+}
+
+const numberWordsToDigits: NumberWordMap = {
+  one: 1,
+  two: 2,
+  three: 3,
+  four: 4,
+  five: 5,
+  six: 6,
+  seven: 7,
+  eight: 8,
+  nine: 9,
+};
+
+const regex = /zero|one|two|three|four|five|six|seven|eight|nine|\d/g;
+
+function findFirstNumber(s: string) {
+  const matches = s.match(regex);
+  if (matches) {
+    // console.log('firstNumber Matched -> ', matches);
+    if (matches[0].length > 1) {
+      return numberWordsToDigits[matches[0]].toString();
+    } else {
+      return matches[0].toString();
+    }
+  } else {
+    return 'number not found';
+  }
+}
+
+function findLastNumber(s: string) {
+  let number = '';
+  let length = s.length;
+
+  while (number == '') {
+    const matches = s.slice(length--, s.length).match(regex);
+    if (matches) {
+      // console.log('lastNumber Matched -> ', matches);
+      if (matches[0].length > 1) {
+        return numberWordsToDigits[matches[0]].toString();
+      } else {
+        return matches[0].toString();
+      }
+    }
+  }
+}
+
 try {
   const data = fs.readFileSync('input.txt', 'utf8');
   const lines = data.split(/\r?\n/); // Splitting by newline
 
   const runningTotal = lines
     .map((line) => {
-      const numbers = line.match(/\d/g) || []; // Finds single-digit numbers, defaults to empty array if none
-      const firstNumber = numbers[0] || '0';
-      const lastNumber = numbers[numbers.length - 1] || firstNumber;
+      const firstNumber = findFirstNumber(line);
+      const lastNumber = findLastNumber(line);
+
+      // console.log('firstNumber->', firstNumber, ',lastNumber->', lastNumber);
       return parseInt(firstNumber + lastNumber, 10); // Combines into two-digit number
     })
     .reduce((sum, num) => sum + num, 0); // Accumulates the sum
 
-  console.log(`Running total of all two-digit numbers: ${runningTotal}`);
+  console.log(`Total of all two-digit numbers: ${runningTotal}`);
 } catch (err) {
   console.error(err);
 }
